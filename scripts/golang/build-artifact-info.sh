56f0b7f352488938ff19b61d6e378cf8079b9f04
Anamika Agrawal <anamika.agrawal@ibm.com>
2021-04-05 22:02:26 +0530

    .

--- scripts/golang/build-artifact-info.sh
+++ scripts/golang/build-artifact-info.sh
@@ -1,6 +1,8 @@
 
 cd binaries	
 
+mv go-gin-app ${APPNAME}
+
 OBJECTNAME=binary-`date '+%Y%m%d%H%M%S'`.tar.gz
 tar -cf ${OBJECTNAME} *
 echo "Object Name of the archive prepared for upload is" ${OBJECTNAME}
