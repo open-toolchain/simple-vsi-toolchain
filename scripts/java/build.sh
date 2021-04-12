992e5924caf573676977319bc17d4da32c9e2475
Anamika Agrawal <anamika.agrawal@ibm.com>
2021-04-12 14:02:29 +0530

    move scripts folder to application repos

--- scripts/java/build.sh
+++
@@ -1,3 +0,0 @@
-echo “Started compiling the source code.”
-mvn -Dmaven.repo.local=./m2/repository clean compile
-echo “Finished compiling the source code.”
--- scripts/java/code-coverage.sh
+++
@@ -1,1 +0,0 @@
-CODE_COVERAGE_RESULTS_LOC=./test-results/site/jacoco/*.xml
