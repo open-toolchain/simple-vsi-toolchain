1af2202250581f42648895ec83b3ca46651c6a0b
Anamika Agrawal <78848657+anamikaagrawal123@users.noreply.github.com>
2021-04-12 16:51:43 +0530

    Scripts-repo and path (#31)
    * script-repo update

--- scripts/golang/acceptance-tests.sh
+++
@@ -1,7 +0,0 @@
-######################################################################################
-# File: acceptance-tests.sh                                                          #
-# Description: This File contains all the acceptance tests for the Deployed app.     #
-#              Add the acceptance task here.                                         #
-######################################################################################
-echo "Running acceptance tests"
-exit 0
--- scripts/java/acceptance-tests.sh
+++
@@ -1,6 +0,0 @@
-######################################################################################
-# File: acceptance-tests.sh                                                          #
-# Description: This File contains all the acceptance tests for the Deployed app      #
-######################################################################################
-echo "Running acceptance tests"
-exit 0
